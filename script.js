238f6eb57835239869c0ece8e2e58658b9322e8d
Cypert-Bon <108354734+cypert-bon@users.noreply.github.com>
2022-12-29 19:19:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
+const form = document.querySelector("form"),
 
+statusTxt = form.querySelector(".button-area span");
 
-      var httpRequest = new XMLHttpRequest();
-      
-      function sendGetRequest() {
-         httpRequest.onreadystatechange = writeContent;
-      
-         httpRequest.open('GET', 'https:/\/codeliber.com/test-server.php');
-         httpRequest.send();
-      }
-      
-      function writeContent() {
-         if(httpRequest.readyState === 4) {
-            if(httpRequest.status == 200) {
-               var response = httpRequest.responseText;
-               document.getElementById("demo").innerHTML = response;
-            } else {
-               alert('There was a problem with the request.');
-            }
-         } else {
-            // response not ready yet
-         }
-      }
+form.onsubmit = (e)=>{
 
+e.preventDefault();
 
+statusTxt.style.color = "#0D6EFD";
 
+statusTxt.style.display = "block";
 
+statusTxt.innerText = "Sending your message...";
 
+form.classList.add("disabled");
 
+let xhr = new XMLHttpRequest();
 
+xhr.open("POST", "message.php", true);
 
+xhr.onload = ()=>{
 
+if(xhr.readyState == 4 && xhr.status == 200){
 
+let response = xhr.response;
 
+if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
 
+statusTxt.style.color = "red";
 
+}else{
 
+form.reset();
 
+setTimeout(()=>{
 
+statusTxt.style.display = "none";
 
+}, 3000);
 
+}
 
+statusTxt.innerText = response;
+
+form.classList.remove("disabled");
+
+}
+
+}
+
+let formData = new FormData(form);
+
+xhr.send(formData);
+
+}
 
